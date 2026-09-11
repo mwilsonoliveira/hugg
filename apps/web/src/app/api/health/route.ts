e9ccf6bb56f4f1e47bd884e040175f71f8a55cfc
mwilsonoliveira <mwilson.oliveira@gmail.com>
2026-09-11 17:38:57 -0300

    fix: Inserindo force-dynamic

--- apps/web/src/app/api/health/route.ts
+++ apps/web/src/app/api/health/route.ts
@@ -1,6 +1,8 @@
 import { NextResponse } from "next/server";
 import { prisma } from "@hugg/database";
 
+export const dynamic = "force-dynamic";
+
 export async function GET() {
   try {
     await prisma.$queryRaw`SELECT 1`;
